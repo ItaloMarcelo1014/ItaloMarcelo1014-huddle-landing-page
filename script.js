73a73d1b110bc19bbb7dcfe294f80148d1d16cbf
Italo Marcelo <marcelo2024.italo20192hotmail.com>
2022-12-02 18:00:48 -0300

    huddle-landing

---
+++ script.js
@@ -0,0 +1,5 @@
+function validate(){
+    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    let email = document.getElementById('email').value;
+    let text = document.getElementById('span-txt');
+}
